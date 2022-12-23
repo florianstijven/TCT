f323c4bc3b1ca1861338bdefc0d4cb47eca1f8aa
florianstijven <florian.stijven@gmail.com>
2022-12-23 15:57:44 +0100

    Added some of the functions to analyze data.

---
+++ R/analyze_trials.R
@@ -0,0 +1,60 @@
+
+#' Analyze a list of trials
+#'
+#' The [analyze_trials()] function analyzes a list of data frames where each
+#' data frame corresponds to a single clinical trial. The fitted-model object
+#' for each trial is returned as a list of model-fit objects. That list can
+#' further be used to extract the relevant estimates or p-values for each trial.
+#'
+#' @param list_data_trials A list of data frames
+#' @param trial_function A function that takes the data frame as argument, fits
+#'   a model, and returns the corresponding fitted-model object.
+#' @param n_cores The number of cores used for parallel computing. If
+#' `n_cores = 1`, no parallel computng is performed.
+#' @param ... Additional parameters that are passed on to `trial_function()`.
+#'
+#' @return A list of fitted-model objects.
+#' @export
+#'
+#' @examples
+analyze_trials = function(list_data_trials,
+                          trial_function,
+                          n_cores = 1,
+                          ...) {
+  if (n_cores == 1) {
+    fitted_models = lapply(
+      X = list_data_trials,
+      FUN = function(x, ...) {
+        tryCatch(
+          expr = trial_function(x, ...),
+          error = function(e) {
+            return(NA)
+          }
+        )
+      },
+      ...
+    )
+  }
+  else {
+    cl = parallel::makeCluster(n_cores)
+    # parallel::clusterExport(cl = cl, varlist = env_vars)
+    parallel::clusterEvalQ(cl = cl, library(tidyverse))
+    fitted_models = parallel::clusterApply(
+      cl = cl,
+      x = list_data_trials,
+      fun = function(x, ...) {
+        tryCatch(
+          expr = trial_function(x, ...),
+          error = function(e) {
+            return(NA)
+          }
+        )
+      },
+      ...
+    )
+    parallel::stopCluster(cl)
+  }
+  return(fitted_models)
+}
+
+
---
+++ R/mmrm.R
@@ -0,0 +1,49 @@
+#' Analyze trial data with MMRM
+#'
+#' The [analyze_mmrm()] function analyzes data from a single trial with the
+#' mixed model for repeated measures (See Details).
+#'
+#' @param data_trial A data frame from a single clinical trial. The following
+#'   variables should be present:
+#'   * `ADAScog_integer`: integer-valued ADAScog score
+#'   * `arm_time`: factor that characterizes the treatment arm-time combination.
+#'   * `time_int`: integer that indicates the measurement occasion. Should start
+#'   with `1` for the first occasion, values for subsequent occasions should be
+#'   consecutive integers.
+#'   *
+#' @param method Which method should be used for fitting the MMRM? Should be one
+#'   of the following strings:
+#'   * `"ML"` (default): maximum likelihood
+#'   * `"REML"`: restricted maximum likelihood
+#'
+#' @return fitted-model object from [nlme::gls()]
+#' @export
+#'
+#' @examples
+analyze_mmrm = function(data_trial, method = "ML") {
+  # The following options could be elaborated on in the future.
+  baseline = FALSE
+  covariates = FALSE
+  if (baseline) {
+    # Transform data to "baseline format".
+    data_trial = baseline_data_format(data_trial)
+    formula = formula(ADAScog_integer~arm_time + b_ADAScog_integer + 0)
+  }
+  else {
+    formula = formula(ADAScog_integer~arm_time + 0)
+  }
+
+  if (covariates) {
+    formula = update.formula(old = formula, .~. +
+                               BMMSE_integer + BMMSE_integer:as.factor(time_int)
+    )
+  }
+
+  nlme::gls(
+    formula,
+    data = data_trial,
+    correlation = nlme::corSymm(form = ~ time_int | SubjId),
+    weights = nlme::varIdent(form = ~ 1 | time_int),
+    method = method
+  )
+}
