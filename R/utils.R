c1f7d20d6ff96c96f16e9c430166b52e475d904e
florianstijven <florian.stijven@gmail.com>
2023-01-06 13:44:32 +0100

    added linear function for extrapolation when interpolating splines are used

--- R/utils.R
+++ R/utils.R
@@ -33,46 +33,52 @@ get_new_time = function(y_ref, x_ref, y_obs, method = "linear") {
   # Only the first first value in y_obs is used. This enables us to use
   # straightforward data handling methods from dplyr.
   y_obs = y_obs
-  if (method == "linear") {
-    ref_fun = function(x,
-                       .x_ref = x_ref,
-                       .y_ref = y_ref) {
-      if ((x < min(.x_ref))) {
-        # standard function does not do extrapolation, this extrapolation is
-        # implemented here.
-        y2 = .y_ref[2]; y1 = .y_ref[1]
-        x2 = .x_ref[2]; x1 = .x_ref[1]
-        # slope
-        a = (y2 - y1) / (x2 - x1)
-        # intercept
-        b = y2 - a * x2
-        y_interpolated = a * x + b
-      }
-      else if (x > max(.x_ref)) {
-        p = length(.x_ref)
-        y2 = .y_ref[p]; y1 = .y_ref[p - 1]
-        x2 = .x_ref[p]; x1 = .x_ref[p - 1]
-        # slope
-        a = (y2 - y1) / (x2 - x1)
-        # intercept
-        b = y2 - a * x2
-        y_interpolated = a * x + b
-      }
-      else {
+  ref_fun = function(x,
+                     .x_ref = x_ref,
+                     .y_ref = y_ref) {
+    if ((x < min(.x_ref))) {
+      # standard function does not do extrapolation, this extrapolation is
+      # implemented here.
+      y2 = .y_ref[2]
+      y1 = .y_ref[1]
+      x2 = .x_ref[2]
+      x1 = .x_ref[1]
+      # slope
+      a = (y2 - y1) / (x2 - x1)
+      # intercept
+      b = y2 - a * x2
+      y_interpolated = a * x + b
+    }
+    else if (x > max(.x_ref)) {
+      p = length(.x_ref)
+      y2 = .y_ref[p]
+      y1 = .y_ref[p - 1]
+      x2 = .x_ref[p]
+      x1 = .x_ref[p - 1]
+      # slope
+      a = (y2 - y1) / (x2 - x1)
+      # intercept
+      b = y2 - a * x2
+      y_interpolated = a * x + b
+    }
+    else {
+      if (method == "linear") {
         y_interpolated = stats::approx(.x_ref,
                                        .y_ref,
                                        method = "linear",
                                        rule = 1,
                                        xout = x)$y
       }
-      return(y_interpolated)
+      else if (method == "spline") {
+        ref_fun = stats::splinefun(x_ref,
+                                   y_ref,
+                                   method = "natural")
+      }
     }
+    return(y_interpolated)
   }
-  else if (method == "spline") {
-    ref_fun = stats::splinefun(x_ref,
-                        y_ref,
-                        method = "natural")
-  }
+
+
   # x-value that maps to y_obs with the reference function. The reference
   # function represents the reference mean trajectory. To find this x-value, we
   # solve the following equation for x_mapped: f_ref (x_mapped) = y_obs. This is
