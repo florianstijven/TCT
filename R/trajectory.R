5a0731d2fc5fa1bc507f4e90aca0a19936300908
florianstijven <florian.stijven@gmail.com>
2023-02-21 13:46:57 +0100

    added trajectory function. This essentially is a non-linear function that can be used for all sorts of non-linear regression problems.

---
+++ R/trajectory.R
@@ -0,0 +1,21 @@
+#' Trajectory function
+#'
+#' This function returns the value of the trajectory at `t`.
+#'
+#' @param t Time at which the trajectory's value is computed.
+#' @param z Group indicater:
+#'  * `z = 0`: indicates the reference group
+#'  * `z = 1`: indicates the treatment group
+#' @param gamma Acceleration factor parameter.
+#' @param alpha (Estimated) points on the reference profile.
+#' @inheritParams get_new_time
+#'
+#' @return (numeric) value of the trajctory at `t`.
+#' @export
+#'
+#' @examples
+trajectory = function(t, z, gamma, alpha, x_ref, method) {
+  # Reference profile
+  f0 = ref_fun_constructor(x_ref, alpha, method = method)
+  f0(t - z * (1 - gamma) * t)
+}
